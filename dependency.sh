4a6e9bbd48cae84aad20157df3c8b9dc01b38ba2
Samray <samrayleung@gmail.com>
2019-03-25 22:25:09 +0800

    update dependencies

--- dependency.sh
+++ dependency.sh
@@ -6,23 +6,18 @@
 install_javascript_dep(){
     if command -v "npm">/dev/null;
     then
+	# Use taobao mirror
+	npm list cnpm -g || sudo npm install -g cnpm --registry=https://registry.npm.taobao.org
 	echo "npm exists"
 	#run as root
 	echo "start to install emacs  dependecy"
-	#emacs javascript-edit mode (ie js2-mode depedency):
-	#install "tern" to use the auto-completion and documentation features
-	echo "Start to install tern"
-	npm i tern -g
-	#to use the formatting features,install js-beautify
+	#to use the formatting features,install js-beautify if doesn't exist
 	echo "Start to install js-beautify"
-	npm i js-beautify -g
+	cnpm list -g js-beautify || sudo cnpm i js-beautify -g
 	#syntax checker Program;which Flycheck dependens on
-	#  install Pylint for Python
-	echo "start to install Python syntax checker"
-	pip3 install pylint --user
 	# install Eslint for Javascript
 	echo "start to install Javascript synatx checker"
-	sudo npm install eslint -g
+	cnpm list -g eslint || sudo cnpm install eslint -g
 	#install required Python packages for Elpy
 	#if [ $(pip install rope;echo $?) != 0 ]
     fi
@@ -31,60 +26,12 @@ install_python_dep(){
     if command -v pip3 >/dev/null;
     then
 	echo "pip3 exists"
+	# use pypi mirror
+	pip install pip -U
+	pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 	#Either of these
-	echo "Start to install ipython"
-	pip3 install ipython --user
-	echo "Start to install rope "
-	pip3 install rope --user
-	#pip install jedi
-	#Flake8 for code checks;syntax checker as pylint
-	echo "Start to install flake8"
-	pip3 install flake8 --user
-	#importmagic for aotumatic imports as IDE does
-	echo  "Start to install importmagic"
-	pip3 install importmagic --user
-	#To be able to suppress unused imports easily,install autoflake
-	echo "Start to install autoflake"
-	pip3 install autoflake --user
-	#and  autopep8 for automatic PEP8 formatting(which is used to format python code)
-	echo "Start to install autopep8"
-	pip3 install autopep8 --user
-	#and yapf for code formatting
-	echo "Start to install yapf"
-	pip3 install yapf  --user
-	#install virtualenv for jedi (python auto-completion)
-	echo "Start to install virtualenv"
-	pip3 install virtualenv --user
-	#install jedi(anaconda dependencies)
-	echo "Start to install jedi"
-	pip3 install jedi --user
-	#install json-rpc(anaconda dependencies)
-	echo "Start to install json-rpc"
-	pip3 install json-rpc --user
-	#install service_factory(anaconda dependencies)
-	echo "Start to install service_factory"
-	pip3 install service_factory --user
-	#install epc
-	echo "Start to install epc"
-	pip3 install epc --user
-	# install isort
-	echo "Start to install isort"
-	pip3 install isort --user
-	# install virtualenvwrapper
-	echo "Start to install virtualenvwrapper"
-	pip3 install virtualenvwrapper --user
-	# install python-language-server
-	pip3 install python-language-server --user
-
-	# install python-language-server
 	echo "Start to install language server"
 	pip3  install 'python-language-server[all]' --user
-	# install latex dependency -- Pygments
-	pip3 install Pygments --user
-
-	echo "Start to install jupyter"
-	pip3 install jupyter --user
-
     fi
 }
 
@@ -138,16 +85,17 @@ install_rust_dep(){
 	curl https://sh.rustup.rs -sSf | sh
     fi
     echo "Install racer dependency"
-    rustup component add rust-src
-    cargo install racer --force
-    cargo install cargo-edit --force
-    cargo install cargo-script --force
+    rustup component add rls-preview rust-analysis rust-src 
+    # Throw an error if binary exists
+    cargo install racer 
+    cargo install cargo-edit 
+    cargo install cargo-script 
 }
 
 main(){
     install_javascript_dep
     install_python_dep
-    install_go_dep
     install_rust_dep
+    # install_go_dep
 }
 main
