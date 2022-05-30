0b919b92ca4c4dbaba85d1c0bce0377cd00dd88a
Ramsay <ramsayleung@gmail.com>
2022-05-30 23:25:25 +0800

    remove useless functions.

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -8,19 +8,7 @@
   :init (progn
 	  (setq geiser-active-implementations '(mit))
 	  (add-hook 'scheme-mode-hook 'geiser-mode)
-	  (add-hook 'geiser-mode-hook #'ramsay/scheme-run-repl-for-code-complete-startup)
 	  )
-  )
-
-(defun ramsay/scheme-run-repl-for-code-complete-startup ()
-  "Run scheme repl for code auto-complete in the startup."
-  (interactive)
-  (if (get-buffer "* Mit REPL *") (message "Mit Repl has launched")
-    (save-excursion
-      (progn
-	(run-mit)
-	(delete-window)
-	)))
   )
 
 (message "loading init-scheme")
