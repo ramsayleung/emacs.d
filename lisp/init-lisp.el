60dc75bd4025d7f87f3ca83672082f39fa8c863d
Ramsay Leung <ramsayleung@gmail.com>
2023-01-12 19:53:17 +0800

    update scheme configuration.

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -14,8 +14,9 @@
 	  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
 	  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 	  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+	  (add-hook 'racket-mode-hook           #'enable-paredit-mode)
           ;;; Auto complete pair symbol, such as `()`, `{}`
-	  (dolist (hook '(emacs-lisp-mode-hook lisp-mode-hook scheme-mode-hook lisp-interaction-mode-hook python-mode-hook rust-mode-hook c++-mode-hook))
+	  (dolist (hook '(emacs-lisp-mode-hook lisp-mode-hook scheme-mode-hook lisp-interaction-mode-hook python-mode-hook rust-mode-hook c++-mode-hook racket-mode-hook))
  	    (add-hook hook 'electric-pair-mode))
 	  )
   )
--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -2,21 +2,15 @@
 ;;; code:
 ;;; commentary:
 
-(use-package geiser
-  :ensure t
-  :defer t
-  :init
-  (setq geiser-active-implementations '(racket))
-  (add-hook 'scheme-mode-hook 'geiser-mode)
-  (use-package geiser-racket
-    :ensure t
-    :defer t)
-  )
+(use-package scheme-mode
+  ;; I am not sure why `:mode ("\\.rkt\\'" "\\.scm\\'")` doesn't work
+  ;; So I choose to implement it with `':hook`, the tricky way
+  :hook (scheme-mode . racket-mode))
 
 (use-package racket-mode
-  :mode("\\.rkt\\'" . racket-mode)
   :ensure t
-  )
+  :hook (racket-mode . racket-xp-mode)
+  :mode ("\\.rkt\\'" "\\.scm\\'"))
 
 (message "loading init-scheme")
 (provide 'init-scheme)
